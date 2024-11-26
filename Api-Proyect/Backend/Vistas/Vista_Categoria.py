234b1837e251dbb9d6e92b07658e4efb8bbd37d9
Sergio <sergiotutu2812@gmail.com>
2024-11-26 07:50:34 -0500

    Modificando vista de categoria

--- Api-Proyect/Backend/Vistas/Vista_Categoria.py
+++ Api-Proyect/Backend/Vistas/Vista_Categoria.py
@@ -6,18 +6,17 @@ from ..modelos import db, Categoria
 app = Flask(__name__)
 
 # Instaciamos el esquema que vamos a utilizar
-categoria_Schema = categoria_Schema()
+categoria_Schema = CategoriaSchema()
 
 # Procedemos a crear la vista de canciones, es decir una clase que tendra los metodos
 class VistaCategoria(Resource):
     def get(self):
-        return [categoria_Schema.dump(Categoria) for Usuario in Usuario.query.all()]
+        return [categoria_Schema.dump(Categoria) for Categoria in Categoria.query.all()]
     
     def post(self):
         nueva_categoria = Categoria(
             Nombre_Cat = request.json['Nombre_Cat'],\
-            Descripcion_Cat = request.json['Descripcion_Cat'],\
-            subcategorias = request.json['subcategorias']
+            Descripcion_Cat = request.json['Descripcion_Cat']
         )
 
         db.session.add(nueva_categoria)
@@ -26,10 +25,8 @@ class VistaCategoria(Resource):
 
     def put(self, Id_Categoria):
         categoria = Categoria.query.get_or_404(Id_Categoria)
-        categoria.Id_Categoria = request.json.get('Id_Categoria', categoria.Id_Categoria)
         categoria.Nombre_Cat = request.json.get('Nombre_Cat', categoria.Nombre_Cat)
         categoria.Descripcion_Cat = request.json.get('Descripcion_Cat', categoria.Descripcion_Cat)
-        categoria.subcategoria = request.json.get('subcategoria', categoria.subcategoria)
 
         db.session.commit()
         return categoria_Schema.dump(categoria)
