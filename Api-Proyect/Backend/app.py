00bad57cb1c3c34dc8b764705193da8d8619a692
Sergio <sergiotutu2812@gmail.com>
2024-12-06 00:06:35 -0500

    Agregando endpoint de login

--- Api-Proyect/Backend/app.py
+++ Api-Proyect/Backend/app.py
@@ -2,7 +2,7 @@ from Backend import create_app
 from flask_migrate import Migrate
 from .Modelos import db
 from flask_restful import Api
-from .Vistas import VistaSubcategoria, VistaProveedor, VistaRol, VistaCategoria, VistaUsuario
+from .Vistas import VistaSubcategoria, VistaProveedor, VistaRol, VistaCategoria, VistaUsuario, VistaLogIn
 
 app = create_app('default')
 app_context = app.app_context()
@@ -17,6 +17,7 @@ api.add_resource(VistaRol, '/roles')
 api.add_resource(VistaSubcategoria, '/subcategorias', '/subcategorias/<int:Id_Subcategoria>')
 api.add_resource(VistaUsuario, '/usuarios', '/usuarios/<int:Id_Usuario>')
 api.add_resource(VistaCategoria, '/categorias', '/categorias/<int:Id_Categoria>')
+api.add_resource(VistaLogIn, '/login')
 
 
 migrate = Migrate()
