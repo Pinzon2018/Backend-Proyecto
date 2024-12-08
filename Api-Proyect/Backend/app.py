505b54cdca1b27cecefce875f33175c8372f0596
Sergio <sergiotutu2812@gmail.com>
2024-12-07 21:38:19 -0500

    Agregando para generar Superadmin en la app

--- Api-Proyect/Backend/app.py
+++ Api-Proyect/Backend/app.py
@@ -1,10 +1,12 @@
 from Backend import create_app
 from flask_migrate import Migrate
-from .Modelos import db
+from .Modelos import db, Usuario
 from flask_restful import Api
 from .Vistas import VistaSubcategoria, VistaProveedor, VistaRol, VistaCategoria, VistaUsuario, VistaLogin
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from werkzeug.security import generate_password_hash
+import datetime
 
 app = create_app('default')
 app_context = app.app_context()
@@ -28,3 +30,20 @@ api.add_resource(VistaLogin, '/login')
 
 migrate = Migrate()
 migrate.init_app(app, db)
+
+with app.app_context():
+    db.create_all()
+    usuario_superadmin = Usuario.query.filter_by(Nombre_Usu='admin').first()
+    if not usuario_superadmin:
+        hashed_password = generate_password_hash('admin_password')  
+        nuevo_usuario = Usuario(
+            Nombre_Usu='admin',
+            Contraseña_hash=hashed_password,
+            Cedula_Usu='123456789',
+            Email_Usu='admin@example.com',
+            Telefono_Usu='123456789',
+            Fecha_Contrato_Inicio=datetime.datetime.utcnow(),
+            rol=1 
+        )
+        db.session.add(nuevo_usuario)
+        db.session.commit()
