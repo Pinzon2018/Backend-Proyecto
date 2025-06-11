404081acbe784fa9c54e09ddf5b541b3132e69c5
CiberNefty <123023309+CiberNefty@users.noreply.github.com>
2025-06-11 16:11:03 -0500

    Update __init__.py
    esta es la nueva actualizacion de lo que es el archivo de comunicacion con el  gestor db y el jwt

--- Api-Proyect/Backend/__init__.py
+++ Api-Proyect/Backend/__init__.py
@@ -1,22 +1,18 @@
 from flask import Flask
-import cloudinary
+from datetime import timedelta
 
 def create_app(config_name):
     app = Flask(__name__)
-    cloudinary.config(
-        cloud_name='ddvao0cuu',
-        api_key='416747191371556',
-        api_secret='-brPxdfIzrQ6Tjhz0r4SD2lyuww'
-    )
     app.config['SECRET_KEY'] = 'sergio9123541'
     app.config['JWT_SECRET_KEY'] = 'pinzon1234133'
     app.config['JWT_TOKEN_LOCATION'] = 'headers'  
     app.config['JWT_HEADER_NAME'] = 'Authorization'
     app.config['JWT_HEADER_TYPE'] = 'Bearer'
+    # app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=2)
     USER_DB = 'root'
-    PASS_DB = ''
+    PASS_DB = 'Vaio_huawei1998'
     URL_DB = 'localhost'
-    NAME_DB = 'bella_actual_connection'
+    NAME_DB = 'db_front_back_orchiposd'
     FULL_URL_DB = f'mysql+pymysql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
     app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
